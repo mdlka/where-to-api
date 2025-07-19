27b7c5ecd7f7c311997d7f34972010c591119581
mdlka <mogilevskiy499@gmail.com>
2025-07-20 02:08:57 +0300

    Refactor place test

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,28 +1,41 @@
 require "test_helper"
 
 class PlaceTest < ActiveSupport::TestCase
-  test "place latitude should be greater than -90" do
-    place = Place.new(name: "test", latitude: -91, longitude: 0)
-    assert_not place.save
+  setup do
+    @place = places(:default)
   end
 
-  test "place latitude should be less than 90" do
-    place = Place.new(name: "test", latitude: 91, longitude: 0)
-    assert_not place.save
+  test "valid place" do
+    assert @place.valid?
   end
 
-  test "place longitude should be greater than -180" do
-    place = Place.new(name: "test", latitude: 0, longitude: -181)
-    assert_not place.save
+  test "invalid when latitude is less than -90" do
+    @place.latitude = -91.0
+    refute @place.valid?
+    assert_not_nil @place.errors[:latitude]
   end
 
-  test "place longitude should be less than 180" do
-    place = Place.new(name: "test", latitude: 0, longitude: 181)
-    assert_not place.save
+  test "invalid when latitude is greater than 90" do
+    @place.latitude = 91.0
+    refute @place.valid?
+    assert_not_nil @place.errors[:latitude]
   end
 
-  test "place name should be at least 3 chars long" do
-    place = Place.new(name: "t", latitude: 0, longitude: 0)
-    assert_not place.save
+  test "invalid when longitude is less than -180" do
+    @place.longitude = -181.0
+    refute @place.valid?
+    assert_not_nil @place.errors[:longitude]
+  end
+
+  test "invalid when longitude is greater than 180" do
+    @place.longitude = 181.0
+    refute @place.valid?
+    assert_not_nil @place.errors[:longitude]
+  end
+
+  test "invalid when name is less than 3 characters long" do
+    @place.name = "t"
+    refute @place.valid?
+    assert_not_nil @place.errors[:name]
   end
 end
