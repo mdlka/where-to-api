577e382dc8198e1753fafe95c2beb2b9476a4f3f
mdlka <mogilevskiy499@gmail.com>
2025-07-21 03:47:22 +0300

    Fix place tests and remove extra validations

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -9,30 +9,6 @@ class PlaceTest < ActiveSupport::TestCase
     assert @place.valid?
   end
 
-  test "invalid when latitude is less than -90" do
-    @place.latitude = -91.0
-    refute @place.valid?
-    assert_not_nil @place.errors[:latitude]
-  end
-
-  test "invalid when latitude is greater than 90" do
-    @place.latitude = 91.0
-    refute @place.valid?
-    assert_not_nil @place.errors[:latitude]
-  end
-
-  test "invalid when longitude is less than -180" do
-    @place.longitude = -181.0
-    refute @place.valid?
-    assert_not_nil @place.errors[:longitude]
-  end
-
-  test "invalid when longitude is greater than 180" do
-    @place.longitude = 181.0
-    refute @place.valid?
-    assert_not_nil @place.errors[:longitude]
-  end
-
   test "invalid when name is less than 3 characters long" do
     @place.name = "t"
     refute @place.valid?
