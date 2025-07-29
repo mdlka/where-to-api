ab4e3267cd1d9d93889e2e63c8e2f0a292475c04
mdlka <mogilevskiy499@gmail.com>
2025-07-29 05:45:00 +0300

    Update plan places controller

--- app/controllers/api/plan_places_controller.rb
+++ app/controllers/api/plan_places_controller.rb
@@ -6,28 +6,34 @@ class Api::PlanPlacesController < ApplicationController
   end
 
   def show
-    render json: @plan.plan_places.find_by!(place_id: params[:id]), status: :ok
+    render json: @plan.plan_places.find(params[:id]), status: :ok
   end
 
   def create
-    plan_place = PlanPlace.new(plan_place_params.merge(plan_id: @plan.id))
+    plan_place = PlanPlace.new(place_params.merge(plan_id: @plan.id))
 
     if plan_place.save
-      render json: plan_place, status: :created, location: api_plan_place_url(@plan.id, plan_place.place_id)
+      render json: plan_place, status: :created, location: api_plan_place_url(@plan.id, plan_place.id)
     else
       render json: { errors: plan_place.errors.full_messages }, status: :unprocessable_content
     end
   end
 
   def destroy
-    @plan.plan_places.find_by!(place_id: params[:id]).destroy
+    @plan.plan_places.find(params[:id]).destroy
     head :no_content
   end
 
   private
 
-  def plan_place_params
-    params.expect(plan_place: [ :place_id ])
+  def place_params
+    raw_params = params.expect(place: [ :latitude, :longitude ])
+
+    if raw_params[:latitude] && raw_params[:longitude]
+      raw_params[:location] = Geo.point(raw_params.delete(:longitude).to_f, raw_params.delete(:latitude).to_f)
+    end
+
+    raw_params
   end
 
   def set_plan
